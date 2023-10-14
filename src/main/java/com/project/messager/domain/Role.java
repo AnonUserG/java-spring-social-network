fc4dcd3d5b4b471a729fd9acaaefc04c38954364
AnonUserG <nick1029384@gmail.com>
2023-10-15 00:04:32 +0300

    first

---
+++ src/main/java/com/project/messager/domain/Role.java
@@ -0,0 +1,12 @@
+package com.project.messager.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    USER;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
---
+++ src/main/java/com/project/messager/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package com.project.messager.repos;
+
+import com.project.messager.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
